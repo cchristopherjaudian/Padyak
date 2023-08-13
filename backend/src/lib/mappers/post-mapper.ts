72c3e74478d174db614555a5222987b3e34430ef
cliljs <calilchristopher052997@gmail.com>
2023-08-13 10:04:42 +0800

    feat: added new fields for post model and types

--- backend/src/lib/mappers/post-mapper.ts
+++ backend/src/lib/mappers/post-mapper.ts
@@ -1,10 +1,18 @@
 import { v4 as uudiv4 } from "uuid";
+
 type TCreateUpdateMapper = {
   post: string;
   distance: string;
   movingTime: string;
-  location: string;
   uid: string;
+  fromLocation: string;
+  toLocation: string;
+  caption: string;
+  photoUrl: string;
+  fromLong: number;
+  toLong: number;
+  fromLat: number;
+  toLat: number;
 };
 
 class PostMapper {
