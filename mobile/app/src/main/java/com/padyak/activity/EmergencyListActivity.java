996d38693069867200c9229a6cf18a29caa400c4
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2024-01-17 22:37:13 +0800

    feat: Add Emergency Contact Registration UI

---
+++ mobile/app/src/main/java/com/padyak/activity/EmergencyListActivity.java
@@ -0,0 +1,16 @@
+package com.padyak.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.padyak.R;
+
+public class EmergencyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_list);
+    }
+}
