ccd17278d0e954bed1122fbd226c05da6f4bb773
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2024-01-19 21:47:20 +0800

    refactor: Set Contact Fragment Style

--- mobile/app/src/main/java/com/padyak/fragment/ContactSelectFragment.java
+++ mobile/app/src/main/java/com/padyak/fragment/ContactSelectFragment.java
@@ -1,49 +1,66 @@
 package com.padyak.fragment;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.padyak.R;
+import com.padyak.activity.AddEmergencyActivity;
+import com.padyak.activity.EmergencyListActivity;
 
-public class ContactSelectFragment extends Fragment {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
+public class ContactSelectFragment extends DialogFragment {
 
     public ContactSelectFragment() {
 
     }
 
-    public static ContactSelectFragment newInstance(String param1, String param2) {
-        ContactSelectFragment fragment = new ContactSelectFragment();
+    public static ContactSelectFragment newInstance(String title) {
+        ContactSelectFragment frag = new ContactSelectFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        //setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme);
     }
+
+    Button btnSelectContact,txAddManually;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_select, container, false);
+        View v = inflater.inflate(R.layout.fragment_contact_select, container, false);
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+
+        btnSelectContact = v.findViewById(R.id.btnSelectContact);
+        txAddManually = v.findViewById(R.id.txAddManually);
+
+        btnSelectContact.setOnClickListener(l->{
+            EmergencyListActivity.emergencyListActivity.browseContacts();
+            dismiss();
+        });
+        txAddManually.setOnClickListener(l->{
+            Intent intent = new Intent(v.getContext(), AddEmergencyActivity.class);
+            startActivity(intent);
+            dismiss();
+        });
+        return v;
     }
 }
