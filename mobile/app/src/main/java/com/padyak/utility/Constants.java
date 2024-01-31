7ab491a0d4d5c3f2b99d33e6c45342639817f2c5
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2024-01-31 17:46:30 +0800

    refactor: Move Alert Maps in Constants

--- mobile/app/src/main/java/com/padyak/utility/Constants.java
+++ mobile/app/src/main/java/com/padyak/utility/Constants.java
@@ -1,5 +1,8 @@
 package com.padyak.utility;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
     public static String baseURL =  "https://v2-xzkjwxqioa-de.a.run.app/";
     public static String userURL = baseURL.concat("users");
@@ -9,4 +12,12 @@ public class Constants {
     public static String locationURL = baseURL.concat("locations");
     public static String alertURL = baseURL.concat("alerts");
     public static String contactURL = baseURL.concat("contacts");
+    public static final Map<Integer,String> alertMap = new HashMap<Integer, String>()
+    {{
+        put(1,"I am safe i just can't maintain the pace.");
+        put(2,"My bike have a problem");
+        put(3,"I had a bike breakdown and  I don't have any tools. Please help");
+        put(4,"I had an accident and I need help.");
+    }};
+
 }
