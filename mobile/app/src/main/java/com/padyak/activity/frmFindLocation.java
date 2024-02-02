290ed63c06f82f3673bd8d090d7bcbf63bcaf051
bryannikkovbarata <98539264+bryannikkovbarata@users.noreply.github.com>
2024-02-02 23:21:59 +0800

    fix: Rating stars, Point nearest location

--- mobile/app/src/main/java/com/padyak/activity/frmFindLocation.java
+++ mobile/app/src/main/java/com/padyak/activity/frmFindLocation.java
@@ -11,8 +11,10 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,12 +46,13 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class frmFindLocation extends AppCompatActivity implements OnMapsSdkInitializedCallback, OnMapReadyCallback {
     FusedLocationProviderClient fusedLocationClient;
-    TextView txNearestCategory, txNearestAddress, txFindNearestTitle, txFindTitle, txNearestDistance, txNearestTime;
+    TextView txNearestCategory, txNearestContact, txFindNearestTitle, txFindTitle, txNearestDistance, txNearestTime, txNearestRating;
     Button btnFindDirection;
     String findCategory, findCode;
     LatLng myPos, nearestLatLng;
@@ -61,8 +64,13 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
     String distanceTime = "";
     double distanceTimeRaw = 0d;
     String lowestDistanceTime = "";
+    String ratings;
+    String nearestUuid = "";
     ProgressDialog progressDialog;
-
+    List<FindLocation> findLocationList = new ArrayList<>();
+    ImageView imgRating1, imgRating2, imgRating3, imgRating4, imgRating5;
+    List<ImageView> ratingList;
+    List<Marker> markersList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,11 +79,25 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.mapFind);
         mapFragment.getMapAsync(this);
+        markersList = new ArrayList<>();
+        imgRating1 = findViewById(R.id.imgRating1);
+        imgRating2 = findViewById(R.id.imgRating2);
+        imgRating3 = findViewById(R.id.imgRating3);
+        imgRating4 = findViewById(R.id.imgRating4);
+        imgRating5 = findViewById(R.id.imgRating5);
+
+        ratingList = new ArrayList<>();
+        ratingList.add(imgRating1);
+        ratingList.add(imgRating2);
+        ratingList.add(imgRating3);
+        ratingList.add(imgRating4);
+        ratingList.add(imgRating5);
 
         btnFindDirection = findViewById(R.id.btnFindDirection);
+        txNearestContact = findViewById(R.id.txNearestContact);
         txNearestCategory = findViewById(R.id.txNearestCategory);
         txNearestDistance = findViewById(R.id.txNearestDistance);
-        txNearestAddress = findViewById(R.id.txNearestAddress);
+        txNearestRating = findViewById(R.id.txNearestRating);
         txFindNearestTitle = findViewById(R.id.txFindNearestTitle);
         txNearestTime = findViewById(R.id.txNearestTime);
         txFindTitle = findViewById(R.id.txFindTitle);
@@ -94,7 +116,6 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
                 break;
         }
         txNearestCategory.setText(findCategory);
-        txFindNearestTitle.setText("Nearest " + findCategory);
         txFindTitle.setText("Find " + findCategory);
 
         btnFindDirection.setOnClickListener(v -> {
@@ -122,7 +143,7 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
                             double _lat = location.getLatitude();
                             double _long = location.getLongitude();
                             myPos = new LatLng(_lat, _long);
-                            List<FindLocation> findLocationList = new ArrayList<>();
+                            findLocationList = new ArrayList<>();
                             List<Location> registeredLocations = new ArrayList<>();
                             VolleyHttp volleyHttp = new VolleyHttp("", null, "location", frmFindLocation.this);
                             JSONObject responseJSON = volleyHttp.getJsonResponse(true);
@@ -138,6 +159,8 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
                                         locationClass.setLongitude(locationObject.getDouble("longitude"));
                                         locationClass.setName(locationObject.getString("name"));
                                         locationClass.setPhotoUrl(locationObject.getString("photoUrl"));
+                                        locationClass.setRating(locationObject.getString("ratings"));
+                                        locationClass.setContact(locationObject.getString("contact"));
                                         registeredLocations.add(locationClass);
                                     }
                                 } catch (JSONException e) {
@@ -166,10 +189,22 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
                                     computed = distance.getDouble("value") / 1000;
                                     distanceTimeRaw = duration.getInt("value") / 60 / 60;
                                     distanceTime = "<" + String.format("%.1f", distanceTimeRaw) + " hr(s)";
-                                    if (computed <= 1d) {
+                                    if (computed <= 15d) {
                                         findLocationList.add(
-                                                new FindLocation(l.getLatitude(), l.getLongitude(), l.getName(), l.getPhotoUrl(), distanceTime, l.getId(), computed)
+                                                new FindLocation(l.getLatitude(), l.getLongitude(), l.getName(), l.getPhotoUrl(), distanceTime, l.getId(), computed, l.getRating(), l.getContact())
                                         );
+                                    }
+
+                                    if (lowestDistance == 0d) {
+                                        lowestDistance = computed;
+                                        nearestLocation = l;
+                                        lowestDistanceTime = distanceTime;
+                                    } else {
+                                        if (computed < lowestDistance) {
+                                            lowestDistance = computed;
+                                            nearestLocation = l;
+                                            lowestDistanceTime = distanceTime;
+                                        }
                                     }
                                 } catch (JSONException e) {
                                     Log.d(Helper.getInstance().log_code, "onSuccess: " + e.getMessage());
@@ -177,20 +212,31 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
                             });
                             runOnUiThread(() -> {
                                 progressDialog.dismiss();
+                                markersList = new ArrayList<>();
                                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPos, 15f));
                                 if (findLocationList.size() == 0) {
                                     Toast.makeText(this, "No nearby " + findCategory + " found", Toast.LENGTH_SHORT).show();
                                     finish();
                                 } else {
+
                                     findLocationList.forEach(find -> {
                                         LatLng findLatLng = new LatLng(find.getLatitude(), find.getLongitude());
-                                        gMap.addMarker(new MarkerOptions()
+                                        Marker m = gMap.addMarker(new MarkerOptions()
                                                 .position(findLatLng)
                                                 .title(find.getLocationName())
                                                 .snippet(find.getId())
                                         );
-
+                                        markersList.add(m);
+                                        if (find.getId().equals(nearestLocation.getId())) {
+                                            loadNearestInfo(find, String.valueOf(lowestDistance), lowestDistanceTime);
+                                        }
                                     });
+                                    gMap.addMarker(new MarkerOptions()
+                                            .position(myPos)
+                                            .title("Me")
+                                    );
+                                    Optional<Marker> m = markersList.stream().filter(marker -> marker.getSnippet().equals(nearestLocation.getId())).findFirst();
+                                    m.ifPresent(Marker::showInfoWindow);
                                 }
                             });
                         } else {
@@ -207,11 +253,53 @@ public class frmFindLocation extends AppCompatActivity implements OnMapsSdkIniti
 
     }
 
+    public void loadNearestInfo(FindLocation location, String distance, String travelTime) {
+        for (int i = 0; i < ratingList.size(); i++) {
+            ratingList.get(i).setVisibility(View.INVISIBLE);
+        }
+        int ratings = Integer.parseInt(location.getRating());
+        LatLng findLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+        Picasso.get().load(location.getPhotoUrl()).into(imgNearestImage);
+        txNearestDistance.setText(distance.concat("km"));
+        txNearestTime.setText(travelTime);
+        txFindNearestTitle.setText(location.getLocationName());
+        txNearestRating.setText(String.valueOf(ratings));
+        txNearestContact.setText(location.getContact());
+        nearestLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        for (int i = 0; i < Integer.parseInt(location.getRating()); i++) {
+            ratingList.get(i).setVisibility(View.VISIBLE);
+        }
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(findLatLng, 15f));
+        Optional<Marker> m = markersList.stream().filter(marker -> marker.getSnippet().equals(location.getId())).findFirst();
+        m.ifPresent(Marker::showInfoWindow);
+
+        btnFindDirection.setEnabled(true);
+    }
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         gMap = googleMap;
         gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         loadNearest();
+
+        gMap.setOnMarkerClickListener(marker -> {
+            if (marker.getSnippet() != null) {
+                String markerId = marker.getSnippet();
+                Optional<FindLocation> findLocationOptional = findLocationList.stream()
+                        .filter(findLocation -> findLocation.getId().equals(markerId))
+                        .findFirst();
+                if (findLocationOptional.isPresent()) {
+                    FindLocation findLocation = findLocationOptional.get();
+
+                    String locationDistance = String.valueOf(findLocation.getDistance());
+                    String locationTime = findLocation.getTravelTime();
+                    loadNearestInfo(findLocation, locationDistance, locationTime);
+                }
+            } else{
+                btnFindDirection.setEnabled(false);
+            }
+            return false;
+        });
     }
 
     @Override
